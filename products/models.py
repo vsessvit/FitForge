8ebed2ddd04376d5babce7d24f7491d51f53796f
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-03 18:24:04 +0000

    Create ProductCategory model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """Model for product categories (Apparel, Equipment, Supplements)"""
+    name = models.CharField(max_length=100, unique=True)
+    friendly_name = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+    
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name

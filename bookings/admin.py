b447135b0bce833d49e61c926fcbc182a7f2d057
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-03 18:28:09 +0000

    Register Booking model in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'class_schedule', 'booking_date', 'status')
+    list_filter = ('status', 'booking_date')
+    search_fields = ('user__username', 'user__email', 'class_schedule__fitness_class__name')
+    date_hierarchy = 'booking_date'
+    readonly_fields = ('booking_date',)

9ffefa280134e9f9c9474325994faf2be5092258
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-03 18:30:40 +0000

    Register UserProfile in admin with search and filters

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'date_of_birth', 'emergency_contact_name')
+    search_fields = ('user__username', 'user__email', 'phone_number', 'emergency_contact_name')
+    list_filter = ('date_of_birth',)
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user', 'phone_number', 'date_of_birth')
+        }),
+        ('Fitness Information', {
+            'fields': ('fitness_goals',)
+        }),
+        ('Emergency Contact', {
+            'fields': ('emergency_contact_name', 'emergency_contact_phone')
+        }),
+    )

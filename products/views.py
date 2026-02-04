27f187c38f6659f53ab1e52d9b809aca24c7eebd
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-04 14:20:08 +0000

    Add product sorting
    - Sort by price, name, category
    - Add sort dropdown
    - Handle in view

--- products/views.py
+++ products/views.py
@@ -4,11 +4,13 @@ from .models import Product, ProductCategory
 
 
 def all_products(request):
-    """Display all products with optional category filtering and search"""
+    """Display all products with optional category filtering, search, and sorting"""
     products = Product.objects.all()
     categories = ProductCategory.objects.all()
     current_category = None
     search_query = None
+    sort = None
+    direction = None
     
     # Search functionality
     if 'q' in request.GET:
@@ -25,11 +27,33 @@ def all_products(request):
         products = products.filter(category__name=category)
         current_category = ProductCategory.objects.filter(name=category).first()
     
+    # Sorting functionality
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
+        
+        if sortkey == 'name':
+            sortkey = 'name'
+        elif sortkey == 'price':
+            sortkey = 'price'
+        elif sortkey == 'category':
+            sortkey = 'category__name'
+        
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+        
+        products = products.order_by(sortkey)
+    
+    current_sorting = f'{sort}_{direction}'
+    
     context = {
         'products': products,
         'categories': categories,
         'current_category': current_category,
         'search_query': search_query,
+        'current_sorting': current_sorting,
     }
     
     return render(request, 'products/all_products.html', context)

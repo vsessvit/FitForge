fdca43b7ba6f468e2d4f1e1adbce06e8bdbf8f19
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-04 11:24:13 +0000

    Create class list view
    - Added all_classes view in classes/views.py
    - Query all FitnessClass objects
    - Pass classes to template context

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import FitnessClass
 
-# Create your views here.
+
+def all_classes(request):
+    """View to show all fitness classes"""
+    classes = FitnessClass.objects.all()
+    
+    context = {
+        'classes': classes,
+    }
+    
+    return render(request, 'classes/all_classes.html', context)

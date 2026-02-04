00847141052910a307b9f53a1176aac13500264f
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-04 14:15:08 +0000

    Create product list view
    - Create all_products view in products/views.py
    - Query all Product objects
    - Pass to template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """Display all products"""
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/all_products.html', context)

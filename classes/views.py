6a8feb14b54d889685baec67ea823dd84e131d3e
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-04 11:57:59 +0000

    Add class category filtering

--- classes/views.py
+++ classes/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from .models import FitnessClass
+from .models import FitnessClass, ClassCategory
 
 
 def all_classes(request):
-    """View to show all fitness classes"""
+    """View to show all fitness classes, with filtering by category"""
     classes = FitnessClass.objects.all()
+    categories = ClassCategory.objects.all()
+    current_category = None
+    
+    if request.GET:
+        if 'category' in request.GET:
+            category_name = request.GET['category']
+            classes = classes.filter(category__name=category_name)
+            current_category = ClassCategory.objects.get(name=category_name)
     
     context = {
         'classes': classes,
+        'categories': categories,
+        'current_category': current_category,
     }
     
     return render(request, 'classes/all_classes.html', context)

0ab32f1250662468e3315dec69f396964fb37b2b
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-04 14:20:48 +0000

    Create product detail view
    - Create product_detail view
    - Get product by id
    - Handle 404

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Product, ProductCategory
 
@@ -57,3 +57,14 @@ def all_products(request):
     }
     
     return render(request, 'products/all_products.html', context)
+
+
+def product_detail(request, product_id):
+    """Display a single product's details"""
+    product = get_object_or_404(Product, pk=product_id)
+    
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)

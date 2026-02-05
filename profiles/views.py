66e31ee09f543dbf5bc99f02f92510cbe3e5b668
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-05 15:13:29 +0000

    Create profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserProfile
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """
+    Display the user's profile
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    
+    context = {
+        'profile': profile,
+    }
+    
+    return render(request, 'profiles/profile.html', context)
+

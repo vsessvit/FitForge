c8afa575f4d43e91a936c98cf0b833ea8cfa7c8b
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-05 10:28:52 +0000

    Create update quantity view
    - Created adjust_bag view to update item quantity
    - Update quantity in session
    - Remove item if quantity is 0
    - Redirect to bag page with success message

--- bag/views.py
+++ bag/views.py
@@ -29,3 +29,23 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
+def adjust_bag(request, item_id):
+    """
+    Adjust the quantity of the specified product in the shopping bag
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag:view_bag'))
+
+

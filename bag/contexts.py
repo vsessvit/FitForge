88b71f7d21f0e100e6bcc851a268c9eef94eb9b2
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-05 10:22:34 +0000

    Create bag context processor
    - Created bag_contents context processor function
    - Calculates bag items, total, and product count from session
    - Added to TEMPLATES context_processors in settings

---
+++ bag/contexts.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from django.conf import settings
+from products.models import Product
+
+
+def bag_contents(request):
+    """
+    Context processor to make bag contents available across all templates
+    """
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = Product.objects.get(pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
--- fitforge/settings.py
+++ fitforge/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'memberships.context_processors.user_membership',
+                'bag.contexts.bag_contents',
             ],
         },
     },

d73495c08e702cc17d3ad8ee1976922f39e75fbe
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-05 10:29:33 +0000

    Add update quantity URL

--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,5 @@ app_name = 'bag'
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
 ]

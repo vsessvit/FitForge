af0c743d50f12bf6013fc2e6dbd2fa8136643a22
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 16:01:19 +0000

    Create payment_intent.succeeded handler
    - Create handle_payment_intent_succeeded method
    - Get payment intent from event
    - Log success to console
    - Return HTTP 200 response

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,3 +14,14 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        print('Payment Intent Succeeded!')
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)

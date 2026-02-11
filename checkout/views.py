1b48e740aa1842182cf03ec3bcc0f39cb7b9f14d
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 15:30:40 +0000

    Create checkout view structure
    - Create checkout view function
    - Get bag from session
    - Redirect to products if bag empty
    - Initialize OrderForm

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    """Handle checkout process"""
+    bag = request.session.get('bag', {})
+    
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect('products:product_list')
+    
+    order_form = OrderForm()
+    
+    context = {
+        'order_form': order_form,
+    }
+    
+    return render(request, 'checkout/checkout.html', context)

dd0b19d8fb8e30e6701f660de8c5a39f44daef60
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 16:05:33 +0000

    Call cache_checkout_data from JavaScript

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,6 +5,7 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('create_payment_intent/', views.create_payment_intent, name='create_payment_intent'),
     path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
     path('wh/', views.webhook, name='webhook'),
--- static/js/checkout.js
+++ static/js/checkout.js
@@ -60,40 +60,74 @@ form.addEventListener('submit', function(ev) {
     // Get CSRF token
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     
-    // Get client secret from server
+    // First, get the client secret from server
     $.ajax({
         url: '/checkout/create_payment_intent/',
         type: 'POST',
         headers: {'X-CSRFToken': csrfToken},
-        success: function(response) {
-            // Confirm card payment with Stripe
-            stripe.confirmCardPayment(response.clientSecret, {
-                payment_method: {
-                    card: card,
-                }
-            }).then(function(result) {
-                if (result.error) {
-                    // Show error to customer
-                    var errorDiv = document.getElementById('card-errors');
-                    var html = `
-                        <span class="icon" role="alert">
-                            <i class="fas fa-times"></i>
-                        </span>
-                        <span>${result.error.message}</span>
-                    `;
-                    $(errorDiv).html(html);
-                    
-                    // Hide loading overlay
-                    $('.loading-overlay').fadeOut();
-                    
-                    // Re-enable card element and submit button
-                    card.update({ 'disabled': false});
-                    $('#submit-button').attr('disabled', false);
-                } else {
-                    // Payment succeeded, submit form
-                    if (result.paymentIntent.status === 'succeeded') {
-                        form.submit();
-                    }
+        success: function(paymentResponse) {
+            var clientSecret = paymentResponse.clientSecret;
+            
+            // Then cache checkout data in the payment intent
+            var saveInfo = Boolean($('#id-save-info').attr('checked'));
+            var postData = {
+                'csrfmiddlewaretoken': csrfToken,
+                'client_secret': clientSecret,
+                'save_info': saveInfo,
+            };
+            
+            $.ajax({
+                url: '/checkout/cache_checkout_data/',
+                type: 'POST',
+                data: postData,
+                success: function(cacheResponse) {
+                    // Finally, confirm card payment with Stripe
+                    stripe.confirmCardPayment(clientSecret, {
+                        payment_method: {
+                            card: card,
+                            billing_details: {
+                                name: $.trim(form.full_name.value),
+                                phone: $.trim(form.phone_number.value),
+                                email: $.trim(form.email.value),
+                                address: {
+                                    line1: $.trim(form.street_address1.value),
+                                    line2: $.trim(form.street_address2.value),
+                                    city: $.trim(form.town_or_city.value),
+                                    state: $.trim(form.county.value),
+                                    postal_code: $.trim(form.postcode.value),
+                                    country: $.trim(form.country.value),
+                                }
+                            }
+                        }
+                    }).then(function(result) {
+                        if (result.error) {
+                            // Show error to customer
+                            var errorDiv = document.getElementById('card-errors');
+                            var html = `
+                                <span class="icon" role="alert">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>${result.error.message}</span>
+                            `;
+                            $(errorDiv).html(html);
+                            
+                            // Hide loading overlay
+                            $('.loading-overlay').fadeOut();
+                            
+                            // Re-enable card element and submit button
+                            card.update({ 'disabled': false});
+                            $('#submit-button').attr('disabled', false);
+                        } else {
+                            // Payment succeeded, submit form
+                            if (result.paymentIntent.status === 'succeeded') {
+                                form.submit();
+                            }
+                        }
+                    });
+                },
+                error: function(cacheError) {
+                    // Reload page, error will be shown in messages
+                    location.reload();
                 }
             });
         },

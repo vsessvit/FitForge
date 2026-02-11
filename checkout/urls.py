a5bdfa0cc9f14f4de913ce7e118127052e664f67
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 16:04:02 +0000

    Add webhook URL
    - Create webhook URL endpoint at /checkout/wh/
    - CSRF exempt handled in view decorator

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('create_payment_intent/', views.create_payment_intent, name='create_payment_intent'),
     path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
+    path('wh/', views.webhook, name='webhook'),
 ]

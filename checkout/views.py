59adbca020329b2ef341cb13045144273ef1b6c3
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 15:45:01 +0000

    Handle form submission in checkout view
    - Save order on valid form submission
    - Create OrderLineItems from bag (products and memberships)
    - Attach user if authenticated
    - Redirect to checkout_success
    - Handle product/membership not found errors

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from bag.contexts import bag_contents
+from products.models import Product
+from memberships.models import MembershipTier
 from .forms import OrderForm
-from .models import Order
+from .models import Order, OrderLineItem
 import stripe
 
 
@@ -13,13 +15,77 @@ def checkout(request):
     """Handle checkout process"""
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     
-    bag = request.session.get('bag', {})
-    
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect('products:product_list')
-    
-    order_form = OrderForm()
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        membership_id = request.session.get('membership_in_bag', None)
+        
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
+        
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            if request.user.is_authenticated:
+                order.user = request.user
+            order.save()
+            
+            # Create order line items for products
+            for item_id, quantity in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('bag:view_bag'))
+            
+            # Create order line item for membership if present
+            if membership_id:
+                try:
+                    membership = MembershipTier.objects.get(id=membership_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        membership=membership,
+                        quantity=1,
+                    )
+                    order_line_item.save()
+                except MembershipTier.DoesNotExist:
+                    messages.error(request, (
+                        "The membership in your bag wasn't found. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('bag:view_bag'))
+            
+            return redirect(reverse('checkout:checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        
+        if not bag and not request.session.get('membership_in_bag'):
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect('products:product_list')
+        
+        order_form = OrderForm()
     
     context = {
         'order_form': order_form,

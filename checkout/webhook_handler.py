1e19669ec76ec18276b10b3463846768f2b58f36
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 16:00:55 +0000

    Create handle_event method

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,3 +6,11 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)

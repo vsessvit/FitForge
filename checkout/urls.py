72880ae77329f96e3cf545f693d1209f55fc5e3d
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 15:40:20 +0000

    Create checkout success URL

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,4 +6,5 @@ app_name = 'checkout'
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('create_payment_intent/', views.create_payment_intent, name='create_payment_intent'),
+    path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
 ]

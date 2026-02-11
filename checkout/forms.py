0996cfa402bdfa1195fe39e366e22d89dd9ced7b
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-11 15:29:11 +0000

    Create OrderForm

---
+++ checkout/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """Form for checkout order details"""
+    class Meta:
+        model = Order
+        fields = (
+            'full_name', 'email', 'phone_number',
+            'street_address1', 'street_address2',
+            'town_or_city', 'county', 'postcode', 'country',
+        )

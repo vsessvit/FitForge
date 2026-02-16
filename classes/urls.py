fac0abc60cce08c43408828622f3b8bcddc8adae
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-16 16:02:45 +0000

    Add class management URLs

--- classes/urls.py
+++ classes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_classes, name='all_classes'),
+    path('add/', views.add_class, name='add_class'),
+    path('edit/<int:class_id>/', views.edit_class, name='edit_class'),
+    path('delete/<int:class_id>/', views.delete_class, name='delete_class'),
     path('<int:class_id>/', views.class_detail, name='class_detail'),
     path('schedules/', views.class_schedule_list, name='class_schedule_list'),
     path('admin/schedules/', views.admin_schedule_list, name='admin_schedule_list'),

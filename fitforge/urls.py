06fe8a9c7212cb6db6eca9a9149b9f9f9960d596
Svitlana Hranovska <little.vsesvit@gmail.com>
2026-02-24 15:54:46 +0000

    Add custom error handlers for 404 and 500 pages

--- fitforge/urls.py
+++ fitforge/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found, server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,7 @@ urlpatterns = [
     path('bookings/', include('bookings.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Custom error handlers
+handler404 = 'fitforge.views.custom_404'
+handler500 = 'fitforge.views.custom_500'
---
+++ fitforge/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def custom_404(request, exception=None):
+    """Custom 404 error handler"""
+    return render(request, '404.html', status=404)
+
+
+def custom_500(request):
+    """Custom 500 error handler"""
+    return render(request, '500.html', status=500)
